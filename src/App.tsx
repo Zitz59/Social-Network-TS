3fbea1aa516b3e3ec7582fb7c9d07719cee19c9d
Zitz59 <dondongudon@gmail.com>
2022-03-30 15:52:35 +0300

    Add ul li tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <ul>
+                    <li>css</li>
+                    <li>html</li>
+                    <li>js</li>
+                    <li>react</li>
+                </ul>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+
+type MessageType = {
+    message: string
+}
+
+
+function HelloMessage(props: MessageType) {
+    return <h1>{props.message}</h1>
+}
+
+function ByeMessage(props: MessageType) {
+    return <h1>{props.message}</h1>
 }
 
 export default App;
