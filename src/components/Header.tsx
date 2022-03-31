b9cdddf35fe6b7a942622467fd8f76600060d32f
Zitz59 <dondongudon@gmail.com>
2022-03-31 22:09:42 +0300

    14 .Add styles to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./Header.module.css";
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className={styles.header}>
             <img
                 src="https://cdn-icons-png.flaticon.com/512/187/187902.png"
                 alt=""/>
