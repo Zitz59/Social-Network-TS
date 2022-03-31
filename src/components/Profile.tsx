4f40428891cf41e6baeb4d01edd6e175fc2018d4
Zitz59 <dondongudon@gmail.com>
2022-03-31 22:12:03 +0300

    14 .Add styles to Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,21 +1,28 @@
 import React from "react";
+import styles from "./Profile.module.css";
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={styles.content}>
             <div>
                 <img
                     src="https://images.unsplash.com/photo-1633283917235-aecae1d65a44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
                     alt=""/>
             </div>
             <div>
-                <img className='avatar'
+                <img className={styles.avatar}
                      src="https://images.unsplash.com/photo-1618826411640-d6df44dd3f7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
                      alt=""/>
                 avatar+description
             </div>
-            <div>/*My posts*/
-                <div>New post</div>
+            <div>
+                <h3>My posts</h3>
+                <div>
+                    <div>
+                        <textarea name="newPost" id="#" placeholder={'write something new'}></textarea>
+                    </div>
+                    <button>Send</button>
+                </div>
             </div>
             <div>
                 post1
