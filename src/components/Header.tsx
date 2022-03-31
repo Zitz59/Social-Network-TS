f29069208443aef318187c6f4fa38b5871906a0b
Zitz59 <dondongudon@gmail.com>
2022-03-31 18:28:37 +0300

    Refactor Navbar component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://cdn-icons-png.flaticon.com/512/187/187902.png"
+                alt=""/>
+        </header>
+    )
+}
+
+export default Header;
