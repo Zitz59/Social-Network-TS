f8205ce7d675310971924d80115707ebc971fd28
Zitz59 <dondongudon@gmail.com>
2022-04-07 00:32:33 +0300

    19 . Add Routes and new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import MyPosts from "./components/MyPosts/MyPosts";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {Route, Routes} from 'react-router-dom';
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
+import Music from "./components/Music/Music";
+
 
 
 const App = () => {
     return (
+
             <div className="app-wrapper">
                 <Header/>
                 <Navbar/>
+
                 <div className='app-wrapper-content'>
-                    <Profile/>
+                    <Routes>
+                        <Route path="/profile" element={<Profile/>}/>
+                        <Route path="/dialogs" element={<Dialogs/>}/>
+                        <Route path="/news" element={<News/>}/>
+                        <Route path="/music" element={<Music/>}/>
+                        <Route path="/settings" element={<Settings/>}/>
+
+
+                    </Routes>
+                    {/*<MyPosts/>*/}
                 </div>
-                {/*<Dialogs/>*/}
-                {/*<MyPosts/>*/}
+
+
+
             </div>
     );
 }
