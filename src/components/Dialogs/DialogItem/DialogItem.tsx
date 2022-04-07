aa445fd348accdd6900139b4fb9e4aa84c9bb9e1
Zitz59 <dondongudon@gmail.com>
2022-04-07 19:50:26 +0300

    23.Add  Dialog item component + props

---
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from '../Dialogs.module.css';
+
+type DialogItemType = {
+    id:string,
+    name:string
+
+}
+
+const DialogItem = (props:DialogItemType) => {
+    let path = '/dialogs/' + props.id
+
+    return (
+        <div className={styles.dialog + ' ' + styles.active}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+export default DialogItem;
