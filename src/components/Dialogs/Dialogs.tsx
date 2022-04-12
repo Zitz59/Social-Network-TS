4b18a4112ff318331f437d7546e8e141630e4e94
Zitz59 <dondongudon@gmail.com>
2022-04-12 22:04:04 +0300

    26.refactor data , add map method to component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,65 +1,52 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
 import styles from './Dialogs.module.css'
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
-import {MessageType} from './Message/Message';
-import {DialogItemType} from './DialogItem/DialogItem';
 
-type dialogsDataPropsType={
-    dialogsData:Array<DialogItemType>
-    messageData:Array<messageDataType>
+
+export type DialogItemType = {
+    id: number,
+    name: string
 }
 
-
-
-
-type messageDataType={
-    id:number
-    message:MessageType
+export type MessageType = {
+    message: string
 }
 
-
-let dialogsData = [
-    {id:1, name:'Alexey'},
-    {id:2, name:'Anton'},
-    {id:3, name:'Ann'},
-    {id:4, name:'Ksu'},
-    {id:5, name:'Max'},
-    {id:6, name:'Stanz Wizard'},
-    {id:7, name:'Dima'}
+let dialogs = [
+    {id: 1, name: 'Alexey'},
+    {id: 2, name: 'Anton'},
+    {id: 3, name: 'Ann'},
+    {id: 4, name: 'Ksu'},
+    {id: 5, name: 'Max'},
+    {id: 6, name: 'Stanz Wizard'},
+    {id: 7, name: 'Dima'}
 ]
 
-
-let messageData = [
-    {id:1, message:"Hi"},
-    {id:2, message:"Where is my money?"},
-    {id:3, message:"Miss You"},
-    {id:4, message:"Yo Yo"},
-    {id:5, message:"Learn React!"},
-    {id:6, message:"How are you?"},
-    {id:7, message:"Miss You"},
+let messages = [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: 'Where is my money?'},
+    {id: 3, message: 'Miss You'},
+    {id: 4, message: 'Yo Yo'},
+    {id: 5, message: 'Learn React!'},
+    {id: 6, message: 'How are you?'},
+    {id: 7, message: 'Miss You'},
 ]
 
-const Dialogs = (props:dialogsDataPropsType) => {
+let dialogsElements = dialogs.map((dialog) => <DialogItem name={dialog.name} id={dialog.id}/>);
+
+
+let messagesElements = messages.map((message) => <Message message={message.message}/>)
+
+const Dialogs = () => {
     return (
         <div className={styles.dialogs}>
             <div className={styles.dialogItems}>
                 <div className={styles.dialog}>
-                    <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                    <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                    <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                    <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                    <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
-                    <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
-                    <DialogItem name={dialogsData[6].name} id={dialogsData[6].id}/>
-                    <DialogItem name={dialogsData[7].name} id={dialogsData[7].id}/>
-
+                    {dialogsElements}
                 </div>
                 <div className={styles.messages}>
-                    <Message message={messageData[0].message}/>
-                    <Message message={messageData[1].message}/>
-                    <Message message={messageData[2].message}/>
+                    {messagesElements}
                 </div>
             </div>
         </div>
