7b0560b5a90aac2cba5b34ed51d5b3ace0179d44
Zitz59 <dondongudon@gmail.com>
2022-04-12 22:02:06 +0300

    26.Add MyPosts to Profile , refactor css,change image

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,19 +1,16 @@
-import React from "react";
-import styles from "./Profile.module.css";
+import React from 'react';
+import MyPosts from '../MyPosts/MyPosts';
 
 const Profile = () => {
     return (
         <div>
             <div>
                 <img
-                    src="https://images.unsplash.com/photo-1633283917235-aecae1d65a44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                    src="https://images.unsplash.com/photo-1615678815958-5910c6811c25?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
                     alt=""/>
             </div>
             <div>
-                <img className={styles.avatar}
-                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3IUfiPYzFAfU0OdNVBk3cNCfucV5HKcOEmg&usqp=CAU"
-                     alt=""/>
-                <div>avatar+description</div>
+                <MyPosts/>
             </div>
         </div>
     )
