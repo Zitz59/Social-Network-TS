7166d4b04cb5062ee25480d77019282ab5a2e94c
Zitz59 <dondongudon@gmail.com>
2022-04-18 23:32:53 +0300

    31.Add ref for messages

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -5,14 +5,21 @@ import Message from './Message/Message';
 import {DialogsType, MessageType} from '../../redux/state';
 
 
-type DialogsPropsType={
-    dialogs:Array<DialogsType>
-    messages:Array<MessageType>
+type DialogsPropsType = {
+    dialogs: Array<DialogsType>
+    messages: Array<MessageType>
 }
 
-const Dialogs = (props:DialogsPropsType) => {
+const Dialogs = (props: DialogsPropsType) => {
     let dialogsElements = props.dialogs.map((dialog) => <DialogItem name={dialog.name} id={dialog.id}/>);
     let messagesElements = props.messages.map((message) => <Message message={message.message}/>)
+
+    let newMessageElement: React.RefObject<HTMLTextAreaElement> = React.createRef()
+
+    let addMessage = () => {
+        let text = newMessageElement.current?.value
+        alert(text)
+    }
 
     return (
         <div className={styles.dialogs}>
@@ -23,6 +30,15 @@ const Dialogs = (props:DialogsPropsType) => {
                 <div className={styles.messages}>
                     {messagesElements}
                 </div>
+            </div>
+            <div>
+                <textarea ref={newMessageElement} className={styles.addMessageArea} name="newMessage" id="#"
+                          placeholder={'write to somebody'}>
+
+                </textarea>
+            </div>
+            <div>
+                <button onClick={() => addMessage()} className={styles.addMessageBtn}>Send</button>
             </div>
         </div>
     )
