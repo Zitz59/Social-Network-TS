422f7d08075519b1ed54e5ea1525e1e12c91f8b6
Zitz59 <dondongudon@gmail.com>
2022-04-25 23:49:42 +0300

    34. Add  addMEssage,changeNewPostText, updateNewMessage functions and props

--- src/render.tsx
+++ src/render.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import './index.css';
 import App from './App';
-import {addPost, RootStateType} from './redux/state';
+import {addMessage, addPost, changeNewPostText, RootStateType, updateNewMessage} from './redux/state';
 
 
 export let rerenderEntireTree = (state:RootStateType)=> {
@@ -12,6 +12,9 @@ export let rerenderEntireTree = (state:RootStateType)=> {
             <BrowserRouter>
                 <App state = {state}
                      addPost = {addPost}
+                     addMessage={addMessage}
+                     changeNewPostText={changeNewPostText}
+                     updateNewMessage ={updateNewMessage}
                 />
             </BrowserRouter>
         </React.StrictMode>
