9da917f462a61f646fd0220128c55223d32cb00c
Zitz59 <dondongudon@gmail.com>
2022-04-29 21:54:50 +0300

    35. Add subscribe callback

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state';
+import  {subscribe} from './redux/state';
 import {rerenderEntireTree} from './render';
 
-
-rerenderEntireTree(state);
+subscribe(rerenderEntireTree)
 
 
 
