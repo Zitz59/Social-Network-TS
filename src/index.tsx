8087b791c8a3afb9086b1b0827b295cce561449d
Zitz59 <dondongudon@gmail.com>
2022-04-30 15:52:49 +0300

    35.Add imports and functions from render.tsx add callback subscribe from state.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,31 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import  {subscribe} from './redux/state';
-import {rerenderEntireTree} from './render';
+import state, {subscribe} from './redux/state';
+import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
+import './index.css';
+import {addMessage, addPost, changeNewPostText, RootStateType, updateNewMessage} from './redux/state';
 
-subscribe(rerenderEntireTree)
+
+
+let rerenderEntireTree = (state:RootStateType)=> {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App state = {state}
+                     addPost = {addPost}
+                     addMessage={addMessage}
+                     changeNewPostText={changeNewPostText}
+                     updateNewMessage ={updateNewMessage}
+                />
+            </BrowserRouter>
+        </React.StrictMode>
+        ,
+        document.getElementById('root')
+    );
+}
 
 
 
@@ -11,3 +33,5 @@ subscribe(rerenderEntireTree)
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+rerenderEntireTree(state)//state with data to rerender
+subscribe(rerenderEntireTree)//callback to rerender when state will change
