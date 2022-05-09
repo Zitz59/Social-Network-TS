5e66f7b99a6b3268c1acea50f67b934006f37dfb
Zitz59 <dondongudon@gmail.com>
2022-05-09 14:06:43 +0300

    38. Add dispatch to MyPosts (change addPost/changeNewPostText to dispatch)

--- src/components/MyPosts/MyPosts.tsx
+++ src/components/MyPosts/MyPosts.tsx
@@ -1,20 +1,20 @@
 import React, {ChangeEvent} from 'react';
 import styles from './MyPosts.module.css';
 import Post from './Post/Post';
-import {AddPostActionType, ChangeNewPostTextActionType, PostsType} from '../../redux/state';
+import {ActionTypes, PostsType} from '../../redux/state';
 
 
 type MyPostsPropsType = {
     posts: Array<PostsType>
-    addPost: (PostMessage: string) => void
+    // addPost: (PostMessage: string) => void
     newPostText: string
-    changeNewPostText: (newText: string) => void
-    dispatch:(action:AddPostActionType|ChangeNewPostTextActionType)=>void
+    // changeNewPostText: (newText: string) => void
+    dispatch:(action:ActionTypes)=>void
 }
 
 const MyPosts = (props: MyPostsPropsType) => {
     let postElements = props.posts.map((post) =>
-        <Post message={post.message} likesCount={post.likesCount} id={post.id}/>)
+        <Post message={post.message} likesCount={post.likesCount} id={post.id} key={post.id}/>)
 
     let newPostElement: React.RefObject<HTMLTextAreaElement> = React.createRef();
     //let newPostElement = React.createRef<HTMLTextAreaElement>(); - 2й вариант
@@ -26,9 +26,9 @@ const MyPosts = (props: MyPostsPropsType) => {
     }
     let onPostChange = (e:ChangeEvent<HTMLTextAreaElement>) => {
         // let text = newPostElement.current ? newPostElement.current.value : ' ';
-        props.changeNewPostText(e.currentTarget.value)
+        // props.changeNewPostText(e.currentTarget.value)
         // props.changeNewPostText(text);
-        props.dispatch({type:'UPDATE-NEW-POST-TEXT',newText:props.newPostText
+        props.dispatch({type:'UPDATE-NEW-POST-TEXT',newText:e.currentTarget.value
     })}
 
 
