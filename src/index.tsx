cd6c593149d8ecac20121d87598603ffd9b2001f
Zitz59 <dondongudon@gmail.com>
2022-05-09 14:08:24 +0300

    38. Add store and remove props from callback rerenderEntireTree

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,22 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import store  from './redux/state';
-import App from "./App";
+import store from './redux/state';
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
-import { RootStateType} from './redux/state';
 
-
-
-let rerenderEntireTree = (_state:RootStateType)=> {
+let rerenderEntireTree = () => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state = {store.getState()}
-                     addPost = {store.addPost.bind(store)}
-                     addMessage={store.addMessage.bind(store)}
-                     changeNewPostText={store.changeNewPostText.bind(store)}
-                     updateNewMessage ={store.updateNewMessage.bind(store)}
+                <App state={store.getState()}
+                     store={store}
+                     // addPost={store.addPost.bind(store)}
+                     // addMessage={store.addMessage.bind(store)}
+                     // changeNewPostText={store.changeNewPostText.bind(store)}
+                     // updateNewMessage={store.updateNewMessage.bind(store)}
                      dispatch={store.dispatch.bind(store)}
                 />
             </BrowserRouter>
@@ -29,10 +27,10 @@ let rerenderEntireTree = (_state:RootStateType)=> {
 }
 
 
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-rerenderEntireTree(store.getState())//state with data to rerender
+//state with data to rerender
+rerenderEntireTree()
 store.subscribe(rerenderEntireTree)//callback to rerender when state will change
