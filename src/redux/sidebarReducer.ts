503cfcc168e2f7ba57d0078dfd14508a8273fc58
Zitz59 <dondongudon@gmail.com>
2022-05-17 22:43:07 +0300

    42. Add initialState and types to sidebarReducer

--- src/redux/sidebarReducer.ts
+++ src/redux/sidebarReducer.ts
@@ -1,6 +1,28 @@
-export const sidebarReducer = () => {
+import {SidebarType} from './store';
 
-    return
+type InitialStateType = Array <SidebarType>
+
+let initialState: InitialStateType =
+    [{
+        id: 1,
+        name: 'Alexey',
+        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWTetpXuW5y9MgX063lA3pYmNRgIiasWqTow&usqp=CAU'
+    },
+        {
+            id: 2,
+            name: 'Anton',
+            avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHVc48I1DV-C9AqL2887a1emxWmko1RKIWBZp9MQtgGfwENC-J4or7EF8Weg5JAo_nKOY&usqp=CAU'
+        },
+        {
+            id: 3,
+            name: 'Maria',
+            avatar: 'https://media.istockphoto.com/vectors/cartoon-young-girl-face-vector-illustration-of-beautiful-woman-avatar-vector-id923639308?b=1&k=20&m=923639308&s=170667a&w=0&h=dNz54KGP6e7MSDib2X6eGkbw4g-JLSdQaBp3pleK88M='
+        }]
+
+
+export const sidebarReducer = (state = initialState) => {
+
+    return state
 }
 
 export default sidebarReducer;
