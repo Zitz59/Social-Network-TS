42f02913e63dd2ab7b75bfaa0953d78e9e6e6409
Zitz59 <dondongudon@gmail.com>
2022-05-20 19:23:34 +0300

    43. refactor Profile , delete unused props and types

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import MyPosts from '../MyPosts/MyPosts';
 import {ActionTypes, PostsType, StoreType} from '../../redux/store';
 import MyPostsContainer from '../MyPosts/MyPostsContainer';
+import {ReduxStoreType} from '../../redux/redux-store';
 
 
 type ProfilePropsType = {
-    posts: Array<PostsType>
+    // posts: Array<PostsType>
     // addPost: (PostMessage: string) => void
-    newPostText: string
+    // newPostText: string
     // changeNewPostText: (newText: string) => void
-    dispatch:(action:ActionTypes)=>void
-    store:StoreType
+    // dispatch:(action:ActionTypes)=>void
+    store:ReduxStoreType
 
 }
 
 const Profile = (props: ProfilePropsType) => {
+    debugger
     return (
         <div>
             <div>
@@ -27,7 +28,7 @@ const Profile = (props: ProfilePropsType) => {
                                   // posts={props.posts}
                          // addPost={props.addPost}
                          // newPostText={props.newPostText}
-                         //changeNewPostText={props.changeNewPostText}
+                         // changeNewPostText={props.changeNewPostText}
                          // dispatch={props.dispatch}
                  />
             </div>
