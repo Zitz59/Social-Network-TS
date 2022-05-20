eddc3a90a87d245b19d72205574120a8b3823175
Zitz59 <dondongudon@gmail.com>
2022-05-20 19:19:47 +0300

    43. create Container component for MyPosts

--- src/components/MyPosts/MyPostsContainer.tsx
+++ src/components/MyPosts/MyPostsContainer.tsx
@@ -2,15 +2,16 @@ import React, {ChangeEvent} from 'react';
 import {ActionTypes, PostsType, StoreType} from '../../redux/store';
 import {addPostAC, onPostChangeAC} from '../../redux/profileReducer';
 import MyPosts from './MyPosts';
+import {ReduxStoreType} from '../../redux/redux-store';
 
 
 type MyPostsContainerPropsType = {
-    posts: Array<PostsType>
-    newPostText: string
-    dispatch: (action: ActionTypes) => void
-    changeNewPostText: () => void
-    addPost: (newPostText: string) => void
-    store: StoreType
+    // posts: Array<PostsType>
+    // newPostText: string
+    // dispatch: (action: ActionTypes) => void
+    // changeNewPostText: (newText:string) => void
+    // addPost: (newPostText: string) => void
+    store: ReduxStoreType
 }
 
 const MyPostsContainer = (props: MyPostsContainerPropsType) => {
@@ -18,17 +19,17 @@ const MyPostsContainer = (props: MyPostsContainerPropsType) => {
 
 
     let addPost = () => {
-        props.store.dispatch(addPostAC(props.newPostText))
+        props.store.dispatch(addPostAC(state.profilePage.newPostText))
 
     }
-    let onPostChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        props.store.dispatch(onPostChangeAC(e.currentTarget.value))
+    let onPostChange = (text:string) => {
+        props.store.dispatch(onPostChangeAC(text))
     }
 
 
     return (<MyPosts addPost={addPost}
                      changeNewPostText={onPostChange}
-                     posts={props.posts}
+                     posts={state.profilePage.posts}
                      newPostText={state.profilePage.newPostText}/>)
 }
 
