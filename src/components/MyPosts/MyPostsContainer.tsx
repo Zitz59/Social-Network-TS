823d7696ee626659de4f99fdcc5f8de64c434f16
Zitz59 <dondongudon@gmail.com>
2022-05-21 21:06:52 +0300

    44. add StoreContext , delete unused props

--- src/components/MyPosts/MyPostsContainer.tsx
+++ src/components/MyPosts/MyPostsContainer.tsx
@@ -1,36 +1,46 @@
-import React, {ChangeEvent} from 'react';
-import {ActionTypes, PostsType, StoreType} from '../../redux/store';
+import React from 'react';
+import store, {ActionTypes, PostsType, StoreType} from '../../redux/store';
 import {addPostAC, onPostChangeAC} from '../../redux/profileReducer';
 import MyPosts from './MyPosts';
 import {ReduxStoreType} from '../../redux/redux-store';
+import StoreContext from '../../StoreContext';
 
 
-type MyPostsContainerPropsType = {
-    // posts: Array<PostsType>
-    // newPostText: string
-    // dispatch: (action: ActionTypes) => void
-    // changeNewPostText: (newText:string) => void
-    // addPost: (newPostText: string) => void
-    store: ReduxStoreType
-}
+// type MyPostsContainerPropsType = {
+//     // posts: Array<PostsType>
+//     // newPostText: string
+//     // dispatch: (action: ActionTypes) => void
+//     // changeNewPostText: (newText:string) => void
+//     // addPost: (newPostText: string) => void
+//     // store: ReduxStoreType
+// }
 
-const MyPostsContainer = (props: MyPostsContainerPropsType) => {
-    let state = props.store.getState();
-
-
-    let addPost = () => {
-        props.store.dispatch(addPostAC(state.profilePage.newPostText))
-
-    }
-    let onPostChange = (text:string) => {
-        props.store.dispatch(onPostChangeAC(text))
-    }
+const MyPostsContainer = (/*props: MyPostsContainerPropsType*/) => {
 
 
-    return (<MyPosts addPost={addPost}
-                     changeNewPostText={onPostChange}
-                     posts={state.profilePage.posts}
-                     newPostText={state.profilePage.newPostText}/>)
+
+
+    return (
+        <StoreContext.Consumer>
+            {(store) => {
+                let state = store.getState();
+
+                let addPost = () => {
+                    store.dispatch(addPostAC(state.profilePage.newPostText))
+                }
+
+                let onPostChange = (text: string) => {
+                    store.dispatch(onPostChangeAC(text))
+                }
+
+                return<MyPosts addPost={addPost}
+                         changeNewPostText={onPostChange}
+                         posts={store.getState().profilePage.posts}
+                         newPostText={store.getState().profilePage.newPostText}/>
+            }
+        }
+        </StoreContext.Consumer>
+    )
 }
 
 export default MyPostsContainer;
