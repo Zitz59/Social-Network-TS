e5ff4d5ba49c2653b1c498bb25cf921dd4263c0c
Zitz59 <dondongudon@gmail.com>
2022-05-21 21:09:36 +0300

    44. change import path for SidebarType

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Navbar.module.css';
 import {NavLink} from 'react-router-dom';
 import Sidebar from '../Sidebar/Sidebar';
-import {SidebarType} from '../../redux/state';
+import {SidebarType} from '../../redux/store';
 
 type NavbarType = {
     sideBar: Array<SidebarType>
