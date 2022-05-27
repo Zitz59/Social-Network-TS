1e616fbcd8861f6e67bf7996468acb2086b5a752
Zitz59 <dondongudon@gmail.com>
2022-05-27 12:01:36 +0300

    45.update imports

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import {SidebarType} from '../../redux/store';
+
 import SideBarItem from './SidebarItem/SideBarItem';
+import {SidebarType} from '../../redux/sidebarReducer';
 
 
 type SidebarPropsType = {
