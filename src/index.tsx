d73da7e96e0ac6db3f2eb80aa19292de99de9ad4
Zitz59 <dondongudon@gmail.com>
2022-05-27 11:54:45 +0300

    45. add imports form react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/redux-store';
+
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
-import {Provider} from './StoreContext';
+import {Provider} from "react-redux"
+import store from './redux/redux-store';
+
 
 let rerenderEntireTree = () => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
                 <Provider store={store}>
-                <App state={store.getState()}
+                <App
+                    state={store.getState()}
                      store={store}
                      dispatch={store.dispatch.bind(store)}
                 />
