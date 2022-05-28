557baa688b54efa9fe62cee8c562a2ecb931af83
Zitz59 <dondongudon@gmail.com>
2022-05-28 20:59:57 +0300

    47.delete renderEntireTree and subscriber

--- src/index.tsx
+++ src/index.tsx
@@ -10,28 +10,28 @@ import {Provider} from "react-redux"
 import store from './redux/redux-store';
 
 
-let rerenderEntireTree = () => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <Provider store={store}>
+// let rerenderEntireTree = () => {
+// }
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
                 <App
                     state={store.getState()}
-                     store={store}
-                     dispatch={store.dispatch.bind(store)}
+                    store={store}
+                    dispatch={store.dispatch.bind(store)}
                 />
-                </Provider>
-            </BrowserRouter>
-        </React.StrictMode>
-        ,
-        document.getElementById('root')
-    );
-}
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>
+    ,
+    document.getElementById('root')
+);
 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-rerenderEntireTree()//state with data to rerender
-store.subscribe(rerenderEntireTree)// store.subscribe(rerenderEntireTree)//callback to rerender when state will change
+// rerenderEntireTree()//state with data to rerender
+// store.subscribe(rerenderEntireTree)// store.subscribe(rerenderEntireTree)//callback to rerender when state will change
