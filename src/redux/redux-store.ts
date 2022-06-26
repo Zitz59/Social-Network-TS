4a0afe115827dff3923d2be46388a0e713cab2cf
Zitz59 <dondongudon@gmail.com>
2022-06-26 11:47:19 +0300

    add usersReducer to hte RootReducer

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -2,6 +2,7 @@ import {combineReducers, createStore} from "redux";
 import profileReducer from "./profileReducer";
 import dialogsReducer from "./dialogsReducer";
 import sidebarReducer from "./sidebarReducer";
+import {usersReducer} from './users-reducer';
 
 
 
@@ -10,7 +11,8 @@ export type ReduxStoreType = typeof store
 export const rootReducer = combineReducers({
     profilePage:profileReducer,
     dialogsPage:dialogsReducer,
-    sideBar:sidebarReducer
+    sideBar:sidebarReducer,
+    users:usersReducer
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
