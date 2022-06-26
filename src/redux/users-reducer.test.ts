7ba2cc1d730cb084a400dc8a37d723cc044b959f
Zitz59 <dondongudon@gmail.com>
2022-06-26 11:44:53 +0300

    fixed bugs in the test

--- src/redux/users-reducer.test.ts
+++ src/redux/users-reducer.test.ts
@@ -1,4 +1,4 @@
-import {followAC, UsersInitialStateType, usersReducer} from './usersReducer';
+import {followAC, unFollowAC, UsersInitialStateType, usersReducer} from './users-reducer';
 
 test('correct user should be followed', () => {
 
@@ -75,29 +75,15 @@ test('correct user should be unfollowed', () => {
                 fullName: 'Kunigunda',
                 status: 'I am very busy',
                 location: {city: 'Denmark', country: 'København'}
-            },
-            {
-                id: 4,
-                followed: false,
-                fullName: 'John',
-                status: 'Do it yourself',
-                location: {city: 'Texas', country: 'USA'}
-            },
-            {
-                id: 5,
-                followed: true,
-                fullName: 'Elena',
-                status: 'Chill',
-                location: {city: 'Male', country: 'Maldives'}
             }
         ]
     }
-    const action = followAC(2)
+    const action = unFollowAC(3)
 
     const endState = usersReducer(startState, action)
 
-    expect(endState['users'][0].followed).toBe(false)
-    expect(endState['users'][1].followed).toBe(true)
+    expect(endState['users'][0].followed).toBe(true)
+    expect(endState['users'][1].followed).toBe(false)
     expect(endState['users'][3].followed).toBe(false)
 
 })
