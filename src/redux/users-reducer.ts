b2f7ebf46d43edd7cb2b7796153807ef4b4d498c
Zitz59 <dondongudon@gmail.com>
2022-06-28 16:42:28 +0300

    create userReducer

--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -1,5 +1,3 @@
-
-
 export type FollowACType = ReturnType<typeof followAC>
 export type UnFollowACType = ReturnType<typeof unFollowAC>
 export type SetUsersACType = ReturnType<any>
@@ -28,29 +26,6 @@ export type UsersInitialStateType = {
 let initialState: UsersInitialStateType = {
     users: []
 }
-// {
-//     id: 1,
-//     followed: true,
-//     fullName: 'Sohan',
-//     status: 'Learn react !!!',
-//     location: {city: 'Tel-a-Viv', country: 'Israel'}
-// },
-// {
-//     id: 2,
-//     followed: false,
-//     fullName: 'Ivan',
-//     status: 'Drinking vodka and fuck with geese',
-//     location: {city: 'Moscow', country: 'Russia'}
-// },
-// {
-//     id: 3,
-//     followed: true,
-//     fullName: 'Kunigunda',
-//     status: 'I am very busy',
-//     location: {city: 'Denmark', country: 'København'}
-// },
-// {id: 4, followed: false, fullName: 'John', status: 'Do it yourself', location: {city: 'Texas', country: 'USA'}},
-// {id: 5, followed: true, fullName: 'Elena', status: 'Chill', location: {city: 'Male', country: 'Maldives'}}
 
 
 export const usersReducer = (state: UsersInitialStateType = initialState, action: userReducerType): UsersInitialStateType => {
@@ -62,7 +37,7 @@ export const usersReducer = (state: UsersInitialStateType = initialState, action
             return {...state, users: state.users.map(u => u.id === action.userId ? {...u, followed: false} : u)}
 
         case 'SET-USERS':
-            return {...state, users: [...state.users, action.users]}
+            return {...state, users: [...state.users, ...action.users]}
         default:
             return state;
     }
