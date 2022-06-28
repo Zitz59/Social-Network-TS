5dbd92cd7a00acfd93575a5dd356c5fe0c5e2ab9
Zitz59 <dondongudon@gmail.com>
2022-06-28 16:40:46 +0300

    add container component for MyPosts

--- src/components/MyPosts/MyPostsContainer.tsx
+++ src/components/MyPosts/MyPostsContainer.tsx
@@ -1,46 +1,10 @@
 import React from 'react';
-import {addPostAC, InitialStateType, onPostChangeAC, PostsType} from '../../redux/profileReducer';
+import {addPostAC, onPostChangeAC, PostsType} from '../../redux/profileReducer';
 import MyPosts from './MyPosts';
 import {AppStateType} from '../../redux/redux-store';
 // import StoreContext from '../../StoreContext';
 import {connect} from 'react-redux';
 import {Dispatch} from 'redux';
-
-
-// type MyPostsContainerPropsType = {
-//     // posts: Array<PostsType>
-//     // newPostText: string
-//     // dispatch: (action: ActionTypes) => void
-//     // changeNewPostText: (newText:string) => void
-//     // addPost: (newPostText: string) => void
-//     // store: ReduxStoreType
-// }
-
-// const MyPostsContainer = (/*props: MyPostsContainerPropsType*/) => {
-//
-//
-//     return (
-//         <StoreContext.Consumer>
-//             {(store) => {
-//                 let state = store.getState();
-//
-//                 let addPost = () => {
-//                     store.dispatch(addPostAC(state.profilePage.newPostText))
-//                 }
-//
-//                 let onPostChange = (text: string) => {
-//                     store.dispatch(onPostChangeAC(text))
-//                 }
-//
-//                 return <MyPosts addPost={addPost}
-//                                 changeNewPostText={onPostChange}
-//                                 posts={store.getState().profilePage.posts}
-//                                 newPostText={store.getState().profilePage.newPostText}/>
-//             }
-//             }
-//         </StoreContext.Consumer>
-//     )
-// }
 
 export type MapStateToPropsType = {
     posts: Array<PostsType>
