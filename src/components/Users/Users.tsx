a7b0977cd676d7bee6ee8d0afff059d1580ef756
Zitz59 <dondongudon@gmail.com>
2022-07-01 12:52:10 +0300

    54 create classComponent

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,21 +5,64 @@ import userPhoto from '../../assets/images/user.png'
 import axios from 'axios';
 
 
-function Users(props: UsersPropsType) {
-    let getUsers = () => {
-        if (props.usersPage.users.length === 0) {
-            debugger
+// function Users(props: UsersPropsType) {
+//     let getUsers = () => {
+//         if (props.usersPage.users.length === 0) {
+//             debugger
+//             axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+//                 props.setUsers(response.data.items)
+//             })
+//         }
+//     }
+//
+//
+//     return <div className={styles.userBlock}>
+//         <button className={styles.getUserButton} onClick={getUsers}>Get users</button>
+//         {
+//             props.usersPage.users.map(u => <div key={u.id} className={styles.userContainer}>
+//                 <span>
+//                     <div>
+//                         <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={styles.userAvatar}
+//                              alt=""/>
+//                     </div>
+//                     <div>{u.followed
+//                         ? <button onClick={() => {
+//                             props.unfollow(u.id)
+//                         }}>Unfollow</button>
+//                         : <button onClick={() => {
+//                             props.follow(u.id)
+//                         }}>Follow</button>}
+//                     </div>
+//                 </span>
+//                 <span>
+//                     <span><div>{u.name}</div>
+//                         <div>{u.status}</div>
+//                     </span>
+//                     <span><div>{'u.location.country'}</div>
+//                         <div>{'u.location.city'}</div></span>
+//                 </span>
+//             </div>)
+//         }
+//
+//     </div>
+//
+// }
+
+class User extends React.Component<UsersPropsType, User> {
+
+    constructor(props: UsersPropsType) {
+        super(props);
+        if (this.props.usersPage.users.length === 0) {
             axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                props.setUsers(response.data.items)
+                this.props.setUsers(response.data.items)
             })
         }
     }
 
-
-    return <div className={styles.userBlock}>
-        <button className={styles.getUserButton} onClick={getUsers}>Get users</button>
-        {
-            props.usersPage.users.map(u => <div key={u.id} className={styles.userContainer}>
+    render() {
+        return <div className={styles.userBlock}>
+            {
+                this.props.usersPage.users.map(u => <div key={u.id} className={styles.userContainer}>
                 <span>
                     <div>
                         <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={styles.userAvatar}
@@ -27,25 +70,25 @@ function Users(props: UsersPropsType) {
                     </div>
                     <div>{u.followed
                         ? <button onClick={() => {
-                            props.unfollow(u.id)
+                            this.props.unfollow(u.id)
                         }}>Unfollow</button>
                         : <button onClick={() => {
-                            props.follow(u.id)
+                            this.props.follow(u.id)
                         }}>Follow</button>}
                     </div>
                 </span>
-                <span>
+                    <span>
                     <span><div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span><div>{'u.location.country'}</div>
                         <div>{'u.location.city'}</div></span>
                 </span>
-            </div>)
-        }
+                </div>)
+            }
 
-    </div>
-
+        </div>
+    }
 }
 
-export default Users;
+export default User;
