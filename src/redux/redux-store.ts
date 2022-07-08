a70b28fc51eea406c98b50d905ad320f8d05ff7b
Zitz59 <dondongudon@gmail.com>
2022-07-08 23:04:16 +0300

    refactor redux-store

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,22 +1,21 @@
-import {combineReducers, createStore} from "redux";
-import profileReducer from "./profileReducer";
-import dialogsReducer from "./dialogsReducer";
-import sidebarReducer from "./sidebarReducer";
+import {combineReducers, createStore} from 'redux';
+import profileReducer from './profileReducer';
+import sidebarReducer from './sidebarReducer';
 import {usersReducer} from './users-reducer';
-
-
+import {dialogsReducer} from './dialogsReducer';
 
 export type ReduxStoreType = typeof store
 
 export const rootReducer = combineReducers({
-    profilePage:profileReducer,
-    dialogsPage:dialogsReducer,
-    sideBar:sidebarReducer,
-    users:usersReducer
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    sideBar: sidebarReducer,
+    users: usersReducer
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
 
-export const store = createStore(rootReducer);
+let store = createStore(rootReducer);
+
 
 export default store;
