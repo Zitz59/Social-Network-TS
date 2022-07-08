103cd5069aa473ccac75cecf0240f6792c7773b8
Zitz59 <dondongudon@gmail.com>
2022-07-08 23:22:45 +0300

    delete unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,18 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
 import {Route, Routes} from 'react-router-dom';
 import News from './components/News/News';
 import Settings from './components/Settings/Settings';
 import Music from './components/Music/Music';
-import {ActionTypes} from './redux/store';
 import {AppStateType, ReduxStoreType} from './redux/redux-store';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';
 
 export type AppPropsType = {
     store: ReduxStoreType
     state: AppStateType
-    dispatch: (action: ActionTypes) => void
 }
 
 const App = (props: AppPropsType) => {
@@ -27,8 +25,8 @@ const App = (props: AppPropsType) => {
 
             <div className="app-wrapper-content">
                 <Routes>
-                    <Route path="/profile" element={<Profile /*store={props.store}*/ />}/>
-                    <Route path="/dialogs/*" element={<DialogsContainer /*store={props.store}*/ />}/>
+                    <Route path="/profile" element={<ProfileContainer/>}/>
+                    <Route path="/dialogs/*" element={<DialogsContainer/>}/>
                     <Route path="/news" element={<News/>}/>
                     <Route path="/music" element={<Music/>}/>
                     <Route path="/settings" element={<Settings/>}/>
