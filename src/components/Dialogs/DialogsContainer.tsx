5c68b1f2958c158a64e86e03b10f6c5c91651d51
Zitz59 <dondongudon@gmail.com>
2022-07-08 23:11:18 +0300

    refactor MapDispatchToProps and AC

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 
-
-import {addMessageAC, initialStateType, onMessageChangeAC} from '../../redux/dialogsReducer';
+import {addMessage,updateNewMessage} from '../../redux/dialogsReducer';
 import Dialogs from './Dialogs';
 import {connect} from 'react-redux'
 import {AppStateType} from '../../redux/redux-store';
-import {Dispatch} from 'redux';
+import {DialogsInitialStateType} from '../../redux/dialogsReducer';
 
 export type MapStatePropsType = {
-    dialogsPage: initialStateType
+    dialogsPage: DialogsInitialStateType
 }
 
 export type MapDispatchPropsType = {
     addMessage: (dialogMessage: string) => void
-    onMessageChange: (newMessage: string) => void
+    updateNewMessage: (newMessage: string) => void
 }
 
 export type DialogPropsType = MapStatePropsType & MapDispatchPropsType
@@ -22,19 +21,7 @@ let mapStateToProps = (state: AppStateType): MapStatePropsType => {
     return {dialogsPage: state.dialogsPage}
 }
 
-let mapDispatchToProps = (dispatch: Dispatch): MapDispatchPropsType => {
-    return {
-        addMessage: (dialogMessage: string) => {
-            dispatch(addMessageAC(dialogMessage))
-        },
-        onMessageChange: (newMessage: string) => {
-            dispatch(onMessageChangeAC(newMessage))
-        }
-    }
-}
-
-
-const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs);
+const DialogsContainer = connect(mapStateToProps, {addMessage, updateNewMessage})(Dialogs);
 
 
 export default DialogsContainer;
