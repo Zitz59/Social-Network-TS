4c3abc4c7471c1f95ad739a5e662d726883d62a1
Zitz59 <dondongudon@gmail.com>
2022-07-08 23:02:06 +0300

    create ProfileInfo Component

---
+++ src/components/Profile/ProfileInfo.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from '../MyPosts/MyPosts.module.css';
+import {Preloader} from '../common/Preloader/Preloader';
+import {ProfileType} from '../../redux/profileReducer';
+
+type ProfileInfoPropsType = {
+    profile:ProfileType
+}
+
+export const ProfileInfo = (props:ProfileInfoPropsType) => {
+    if (!props.profile) {
+        return <Preloader/>
+    }
+    return (
+        <div>
+            <img className={styles.avatar}
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3IUfiPYzFAfU0OdNVBk3cNCfucV5HKcOEmg&usqp=CAU"
+                alt=""/>
+            <div><img alt={"profile_large_photo"} src={props.profile.photos.large}/>
+                avatar+description</div>
+        </div>
+    )
+}
