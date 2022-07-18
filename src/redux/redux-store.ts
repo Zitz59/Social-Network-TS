40ccb892649f1729b1a84439045050f439652961
Zitz59 <dondongudon@gmail.com>
2022-07-18 14:33:27 +0300

    add authReducer

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,8 +1,9 @@
-import {combineReducers, createStore} from 'redux';
+import {combineReducers,  createStore} from 'redux';
 import profileReducer from './profileReducer';
 import sidebarReducer from './sidebarReducer';
 import {usersReducer} from './users-reducer';
 import {dialogsReducer} from './dialogsReducer';
+import {authReducer} from './auth-reducer';
 
 export type ReduxStoreType = typeof store
 
@@ -10,7 +11,8 @@ export const rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sideBar: sidebarReducer,
-    users: usersReducer
+    users: usersReducer,
+    auth:authReducer
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
