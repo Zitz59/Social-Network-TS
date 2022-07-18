7ba333e58eddf9742bdd8a467ce3bc908431ccce
Zitz59 <dondongudon@gmail.com>
2022-07-18 14:39:59 +0300

    add loginBlock to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import styles from "./Header.module.css";
+import {NavLink} from 'react-router-dom';
 
-const Header = () => {
+type HeaderPropsType = {
+    userId: number,
+    email: string,
+    login: string|null,
+    isAuth: boolean
+}
+
+
+const Header = (props:HeaderPropsType) => {
     return (
         <header className={styles.header}>
             <img
                 src="https://cdn-icons-png.flaticon.com/512/187/187902.png"
                 alt=""/>
+
+            <div className={styles.loginBlock}>
+                {props.isAuth ? props.login
+                : <NavLink to={'/login'}>Login</NavLink>}
+            </div>
         </header>
     )
 }
