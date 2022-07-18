09f6df71771d6f6b87afd9a3824213ba5e67ea0c
Zitz59 <dondongudon@gmail.com>
2022-07-18 14:29:17 +0300

    add MyPosts to MyPostsContainer

--- src/components/MyPosts/MyPostsContainer.tsx
+++ src/components/MyPosts/MyPostsContainer.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
 import {addPost, updatePost, PostsType} from '../../redux/profileReducer';
-import MyPosts from './MyPosts';
 import {AppStateType} from '../../redux/redux-store';
 import {connect} from 'react-redux';
-import {Dispatch} from 'redux';
+import {compose, Dispatch} from 'redux';
+import {ChangeEvent} from 'react';
+import styles from './MyPosts.module.css';
+import Post from './Post/Post';
+
+
+const MyPosts = (props: MyPostsPropsType) => {
+    let postElements = props.posts.map((post) =>
+        <Post message={post.message} likesCount={post.likesCount} id={post.id} key={post.id}/>)
+
+    let newPostElement: React.RefObject<HTMLTextAreaElement> = React.createRef();
+    //let newPostElement = React.createRef<HTMLTextAreaElement>(); - 2й вариант
+
+    let addPost = () => {
+        props.addPost(props.newPostText)
+        // props.dispatch(addPostAC(props.newPostText))
+
+    }
+    let onPostChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        let text = newPostElement.current ? newPostElement.current.value : ' ';
+        props.changeNewPostText(e.currentTarget.value)
+        props.changeNewPostText(text);
+        // props.dispatch(onPostChangeAC(e.currentTarget.value))
+    }
+
+
+    return (
+        <div className={styles.content}>
+            <div className={styles.myPost}>
+                <h3>My posts</h3>
+                <div>
+                    <div>
+                        <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}
+                                  className={styles.addPostArea} name="newPost" id="#"
+                                  placeholder={'write something'}/>
+                    </div>
+                    <button onClick={addPost} className={styles.addPostBtn}>Add post</button>
+                </div>
+            </div>
+            {postElements}
+        </div>
+    )
+}
 
 export type MapStateToPropsType = {
     posts: Array<PostsType>
@@ -38,7 +79,4 @@ let mapDispatchToProps = (dispatch: Dispatch): mapDispatchToPropsType => {
 
 }
 
-const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts)
-
-
-export default MyPostsContainer;
+export default compose<() => JSX.Element>(connect(mapStateToProps, mapDispatchToProps))(MyPosts)
