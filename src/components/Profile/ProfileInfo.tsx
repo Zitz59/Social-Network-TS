8536e3fc289d68614062029001ecd443825e3607
Zitz59 <dondongudon@gmail.com>
2022-07-18 14:32:53 +0300

    edit profileInfo

--- src/components/Profile/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo.tsx
@@ -4,20 +4,23 @@ import {Preloader} from '../common/Preloader/Preloader';
 import {ProfileType} from '../../redux/profileReducer';
 
 type ProfileInfoPropsType = {
-    profile:ProfileType
+    profile: ProfileType
 }
 
-export const ProfileInfo = (props:ProfileInfoPropsType) => {
+export const ProfileInfo = (props: ProfileInfoPropsType) => {
     if (!props.profile) {
         return <Preloader/>
     }
     return (
         <div>
             <img className={styles.avatar}
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3IUfiPYzFAfU0OdNVBk3cNCfucV5HKcOEmg&usqp=CAU"
-                alt=""/>
-            <div><img alt={"profile_large_photo"} src={props.profile.photos.large}/>
-                avatar+description</div>
+                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3IUfiPYzFAfU0OdNVBk3cNCfucV5HKcOEmg&usqp=CAU"
+                 alt=""/>
+            <div>
+                <img alt={'profile_large_photo'} src={props.profile.photos.large}/>
+                <div>About me :{props.profile.aboutMe}</div>
+                <div>fullName : {props.profile.fullName}</div>
+            </div>
         </div>
     )
 }
