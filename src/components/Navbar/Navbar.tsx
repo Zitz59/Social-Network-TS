0a977e4ebe0ad47e15f34c141d4a868b2f5f779b
Zitz59 <dondongudon@gmail.com>
2022-07-18 14:29:49 +0300

    change navLinks for react-router dom 5

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,52 +4,51 @@ import {NavLink} from 'react-router-dom';
 import Sidebar from '../Sidebar/Sidebar';
 import {SidebarType} from '../../redux/sidebarReducer';
 
-
 type NavbarType = {
     sideBar: Array<SidebarType>
 }
 
 const Navbar = (props: NavbarType) => {
     return (
-        <nav className={styles.nav}>
+        <div className={styles.nav}>
             <div className={styles.navItem}>
-                <NavLink to={'/profile'} className={navData => navData.isActive ? styles.active : styles.nav}>
-                    <a>Profile</a>
+                <NavLink to={'/profile'} activeClassName={styles.active} className={styles.nav}>
+                    Profile
                 </NavLink>
             </div>
             <div className={styles.navItem}>
-                <NavLink to={'/dialogs'} /*style={(params)=>{return{color: params.isActive ?'#99D3FF': 'white'}}}*/
-                         className={navData => navData.isActive ? styles.active : styles.nav}>
-                    <a>Messages</a>
+                <NavLink to={'/dialogs/'} /*style={(params)=>{return{color: params.isActive ?'#99D3FF': 'white'}}}*/
+                         activeClassName={styles.active} className={styles.nav}>
+                  Messages
                 </NavLink>
             </div>
             <div className={styles.navItem}>
-                <NavLink to={'/news'} className={navData => navData.isActive ? styles.active : styles.nav}>
-                    <a>News</a>
+                <NavLink to={'/news'} activeClassName={styles.active} className={styles.nav}>
+                    News
                 </NavLink>
             </div>
             <div className={styles.navItem}>
-                <NavLink to="/music" className={navData => navData.isActive ? styles.active : styles.nav}>
-                    <a>Music</a>
+                <NavLink to="/music" activeClassName={styles.active} className={styles.nav}>
+                    Music
                 </NavLink>
             </div>
             <div className={styles.navItem}>
-                <NavLink to="/settings" className={navData => navData.isActive ? styles.active : styles.nav}>
-                    <a>Settings</a>
+                <NavLink to="/settings" activeClassName={styles.active} className={styles.nav}>
+                    Settings
                 </NavLink>
             </div>
             <div className={styles.navItem}>
-                <NavLink to="/users" className={navData => navData.isActive ? styles.active : styles.nav}>
-                    <a>Users</a>
+                <NavLink to="/users" activeClassName={styles.active} className={styles.nav}>
+                    Users
                 </NavLink>
             </div>
             <div className={styles.navItem}>
-                <NavLink to="/friends" className={navData => navData.isActive ? styles.active : styles.nav}>
-                    <a>Friends</a>
+                <NavLink to="/friends" activeClassName={styles.active} className={styles.sidebar_block}>
+                    Friends
                 </NavLink>
                 <Sidebar sidebar={props.sideBar}/>
             </div>
-        </nav>
+        </div>
     )
 }
 
