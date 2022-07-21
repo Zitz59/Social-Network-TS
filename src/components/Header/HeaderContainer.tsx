99f2871cb6d9be9194d3c13c7f4afeba85352f5a
Zitz59 <dondongudon@gmail.com>
2022-07-21 15:24:23 +0300

    refactor:delete debugger

--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -25,7 +25,6 @@ class HeaderContainer extends React.Component<HeaderContainerPropsType, {}> {
     componentDidMount() {
         axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {withCredentials: true})
             .then(response => {
-                debugger
                 if (response.data.resultCode === 0) {
                     let {id, email, login, isAuth} = response.data.data
                     this.props.setAuthUserData(id, email, login, isAuth)
