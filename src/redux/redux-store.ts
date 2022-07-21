93bad158060463b4f3d4fd0844bae44a31e924a3
Zitz59 <dondongudon@gmail.com>
2022-07-21 15:23:54 +0300

    feat: add thunkMiddleware to store

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,9 +1,10 @@
-import {combineReducers,  createStore} from 'redux';
+import {applyMiddleware, combineReducers, createStore} from 'redux';
 import profileReducer from './profileReducer';
 import sidebarReducer from './sidebarReducer';
 import {usersReducer} from './users-reducer';
 import {dialogsReducer} from './dialogsReducer';
 import {authReducer} from './auth-reducer';
+import thunkMiddleware from 'redux-thunk';
 
 export type ReduxStoreType = typeof store
 
@@ -17,7 +18,7 @@ export const rootReducer = combineReducers({
 
 export type AppStateType = ReturnType<typeof rootReducer>
 
-let store = createStore(rootReducer);
+let store = createStore(rootReducer,applyMiddleware(thunkMiddleware));
 
 
 export default store;
