d648b63318a63b9987a09981f8b8cbc7c0c618ba
Zitz59 <dondongudon@gmail.com>
2022-08-10 17:54:09 +0300

    feat:add redux form with types

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,16 +1,48 @@
-import s from './Login.module.css'
+import s from './Login.module.css';
+import {Field, InjectedFormProps, reduxForm} from 'redux-form';
+import React from 'react';
 
-export const Login = () => {
-return(
-    <div className={s.blockContainer}>
-        <div className={s.loginContainer}>
-            <h1 className={s.loginTitle}>Login</h1>
-            <input className={s.input} placeholder={'Login'} type="text"/>
-            <input className={s.input} placeholder={'Password'} type="text"/>
-            <button className={s.button} >Login</button>
-        </div>
-    </div>
-
-)
-
+export type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
 }
+
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+    return (
+        <form className={s.loginContainer} onSubmit={props.handleSubmit}>
+            <div>
+                <Field className={s.input} placeholder={'login'} name={'login'} component={'input'}/>
+            </div>
+            <div>
+                <Field className={s.input} placeholder={'password'} name={'password'} component={'input'}/>
+            </div>
+            <div>
+                <Field className={s.input} component={'input'} name={'rememberMe'} type={'checkbox'}/> remember me
+            </div>
+            <button className={s.button}>Login</button>
+        </form>
+    )
+}
+
+
+const LoginReduxForm = reduxForm<FormDataType>({form: 'login'})(LoginForm)
+
+
+const Login = () => {
+    const onSubmit = (formData: FormDataType) => {
+        console.log(formData)
+    }
+    return (
+
+        <div className={s.blockContainer}>
+            <div className={s.loginContainer}>
+                <h1 className={s.loginTitle}>Login</h1>
+                <LoginReduxForm onSubmit={onSubmit}/>
+            </div>
+        </div>
+
+    )
+}
+
+export default Login
