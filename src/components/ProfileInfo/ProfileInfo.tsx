93e60fa7fa59829c0770b83b133148602f1ed983
Zitz59 <dondongudon@gmail.com>
2022-08-10 14:47:23 +0300

    feat:connect profileStatus

--- src/components/ProfileInfo/ProfileInfo.tsx
+++ src/components/ProfileInfo/ProfileInfo.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import styles from '../MyPosts/MyPosts.module.css';
 import {Preloader} from '../common/Preloader/Preloader';
 import {ProfileType} from '../../redux/profileReducer';
+import {ProfileStatusType} from '../Profile/Profile';
+import {ProfileStatus} from './ProfileStatus/ProfileStatus';
 
 type ProfileInfoPropsType = {
     profile: ProfileType
 }
 
-export const ProfileInfo = (props: ProfileInfoPropsType) => {
+export const ProfileInfo = (props: ProfileInfoPropsType & ProfileStatusType) => {
     if (!props.profile) {
         return <Preloader/>
     }
     return (
         <div>
+            <ProfileStatus updateStatus={props.updateStatus} status={props.status}/>
             <div>
                 <img className={styles.avatar} alt={'profile_large_photo'} src={props.profile.photos.large}/>
                 <div>fullName : {props.profile.fullName}</div>
