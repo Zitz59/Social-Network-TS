a6165c789e01b706a2a31894bec940e5b451b8f7
Zitz59 <dondongudon@gmail.com>
2022-08-10 17:56:49 +0300

    feat:add formReducer for redux-form and window.store for state view in browser

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -5,6 +5,8 @@ import {usersReducer} from './users-reducer';
 import {dialogsReducer} from './dialogsReducer';
 import {authReducer} from './auth-reducer';
 import thunkMiddleware from 'redux-thunk';
+import {reducer as formReducer} from 'redux-form';
+
 
 export type ReduxStoreType = typeof store
 
@@ -13,7 +15,8 @@ export const rootReducer = combineReducers({
     dialogsPage: dialogsReducer,
     sideBar: sidebarReducer,
     users: usersReducer,
-    auth:authReducer
+    auth:authReducer,
+    form:formReducer
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
@@ -22,3 +25,6 @@ let store = createStore(rootReducer,applyMiddleware(thunkMiddleware));
 
 
 export default store;
+
+// @ts-ignore
+window.store = store;
