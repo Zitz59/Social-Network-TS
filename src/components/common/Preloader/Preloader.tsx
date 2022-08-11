3dd7d6ab580044a8a05bec3c05e7c1a5cfed135e
Zitz59 <dondongudon@gmail.com>
2022-08-11 17:31:41 +0300

    feat:add styles

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import preloader from '../../../assets/images/Loading_2.gif';
+import preloader from '../../../assets/images/Loading_3.gif';
+import s from './Preloader.module.css'
 
 export const Preloader = () => {
-    return <div style={{width: '50px', height: '50px'}}>
-        <img src={preloader} alt={"loader"}/>
-    </div>
+    return (
+        <div className={s.preloaderBlock}>
+            <div className={s.preloaderInner}>
+                <img src={preloader} alt={'loader'}/>
+            </div>
+        </div>
+    )
 }
