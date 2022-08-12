42210e978c8e55078e5103e699cc80d4084019fe
Zitz59 <dondongudon@gmail.com>
2022-08-12 23:24:39 +0300

    feat:add Login component

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,34 @@ import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Dialogs from './components/Dialogs/DialogsContainer';
-import PrimarySearchAppBar from './components/AppBar';
+import Login from './components/Login/Login';
 
 export type AppPropsType = {
     store: ReduxStoreType
     state: AppStateType
 }
 
-const App = (props: AppPropsType) => {
-    return (
+class App extends React.Component<AppPropsType> {
+    render() {
+        // @ts-ignore
+        return (
 
-        <div className="app-wrapper">
-            <PrimarySearchAppBar/>
-            <HeaderContainer/>
-            <Navbar sideBar={props.state.sideBar}/>
-            <div className="app-wrapper-content">
+            <div className="app-wrapper">
+                {/*<PrimarySearchAppBar/>*/}
+                <HeaderContainer/>
+                <Navbar sideBar={this.props.state.sideBar}/>
+                <div className="app-wrapper-content">
                     <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
-                    <Route path="/dialogs/*" render={()=><Dialogs/>}/>
-                    <Route path="/news" render={()=><News/>}/>
-                    <Route path="/music" render={()=><Music/>}/>
-                    <Route path="/settings" render={()=><Settings/>}/>
-                    <Route path="/users" render={()=><UsersContainer/>}/>
+                    <Route path="/dialogs/*" render={() => <Dialogs/>}/>
+                    <Route path="/news" render={() => <News/>}/>
+                    <Route path="/music" render={() => <Music/>}/>
+                    <Route path="/settings" render={() => <Settings/>}/>
+                    <Route path="/users" render={() => <UsersContainer/>}/>
+                    <Route path="/login" render={() => <Login/>}/>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
 export default App;
