7568c6974f52ab3d01a7e9e2553a26408667275d
Zitz59 <dondongudon@gmail.com>
2022-08-14 13:13:06 +0300

    feat:add api for authentification

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,17 +1,17 @@
 import axios from 'axios';
 import {UserType} from '../redux/users-reducer';
+import {ProfileType} from '../redux/profileReducer';
 
 type ApiUsersType = {
     items: Array<UserType>
     totalCount: number
     resultCode: number
 }
-
 export const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers: {
-        'API-KEY': '6b74dff9-4a4a-410f-be7c-0e00f1a06ec7'
+        'API-KEY': 'fede4ff7-c313-462c-a587-eaa0340c98b7'
     }
 });
 
@@ -35,9 +35,47 @@ export const usersAPI = {
             })
     },
     unFollowUser(userId: number) {
-        return instance.delete<ApiUsersType>(`follow/${userId}`)
+        return instance.delete<ApiUsersType>(`follow/` + userId)
             .then((response: { data: ApiUsersType; }) => {
+                return response.data
+            })
+    },
+    getProfile(userId: number) {
+        console.log('Obsolete please use profileAPI object')
+        return profileAPI.getProfile(userId)
+    }
+
+}
+
+export const profileAPI = {
+    getProfile(userId: number) {
+        return instance.get<ProfileType>(`profile/` + userId)
+            .then((response) => {
+                return response.data
+            });
+    },
+    getStatus(userId: number) {
+        return instance.get(`profile/status/` + userId)
+            .then((response) => {
+                return response.data
+            })
+    }, updateStatus(status: string) {
+        return instance.put(`profile/status`, {status: status})
+            .then((response) => {
                 return response.data
             })
     }
 }
+
+export const authAPI = {
+    me() {
+        return instance.get(`auth/me`)
+    },
+    login(email: string, password: string, rememberMe?: boolean) {
+        return instance.post(`auth/login`, {email, password, rememberMe})
+    },
+    logout() {
+        return instance.delete(`auth/logout`)
+    }
+}
+
