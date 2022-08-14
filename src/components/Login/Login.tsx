fdc365ed71c51ec71912b453c37d027504d00d62
Zitz59 <dondongudon@gmail.com>
2022-08-14 21:55:00 +0300

    feat: add validation

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,43 +1,58 @@
-import {useForm} from 'react-hook-form';
-import s from './Login.module.css';
+import {SubmitHandler, useForm} from 'react-hook-form';
+import s from './Login.module.scss';
 import React from 'react';
 
 type FormData = {
-    email:string,
-    password:string,
-    rememberMe:boolean
+    email: string,
+    password: string,
+    rememberMe: boolean
 }
 
 export const Login = () => {
-    const {register, setValue, handleSubmit, formState: {errors}} = useForm<FormData>();
-    const onSubmit = (data: FormData) => {
+    const {register, setValue, handleSubmit, formState: {errors}, reset} = useForm<FormData>();
+    const onSubmit: SubmitHandler<FormData> = (data) => {
         console.log('RESULT', data);
         alert(JSON.stringify(data));
+        reset()
+
     };
     console.log(errors);
     return (
         <div className={s.blockContainer}>
             <form className={s.loginContainer} onSubmit={handleSubmit(onSubmit)}>
                 <h1 className={s.loginTitle}>Login</h1>
-                <div>
-                    <input type={'text'}
-                           className={s.input}
-                           placeholder={'email'}
+                <div className={s.input}>
+                    <input className={s.input} type={'text'}
+                           placeholder={'Email'}
                            {...register('email', {
-                               required: true,
-                               pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                               required: 'Please enter your e-mail',
+                               pattern: {
+                                   value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                   message: 'Please enter valid email!'
+                               }
                            })}/>
+                    {errors.email && <div className={s.errorTitle}>{errors.email.message}</div>}
                 </div>
-                <div>
-
+                <div className={s.input}>
                     <input className={s.input}
-                           placeholder={'password'}
-                           {...register('password', {required: true, maxLength: 80})}/>
+                           placeholder={'Password'}
+                           {...register('password',
+                               {
+                                   required: 'Please enter your password',
+                                   maxLength: {
+                                       value: 80,
+                                       message: 'Password max length is 80 symbols'
+                                   }
+                               })}/>
+                    {errors.password &&
+                        <div className={s.errorTitle}>{errors.password.message}</div>}
                 </div>
                 <div>
-                    <input className={s.input} name={'rememberMe'} type={'checkbox'}/> remember me
+                    <input name={'rememberMe'} type={'checkbox'}/> remember me
                 </div>
-                <button className={s.button}>Login</button>
+                <div className={s.buttonBlock}>
+                    <button className={s.button}>Login</button>
+                </div>
             </form>
         </div>
 
