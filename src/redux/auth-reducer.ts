29bfdfc4cd1c489cbddb42f17049c181d1c724e2
Zitz59 <dondongudon@gmail.com>
2022-09-08 20:23:12 +0300

    feat:add stopSubmit action creator and add it to login thunk

--- src/redux/auth-reducer.ts
+++ src/redux/auth-reducer.ts
@@ -1,46 +1,59 @@
 import {Dispatch} from 'redux';
 import {authAPI} from '../api/api';
-import {AppThunk} from "./redux-store";
+import {AppThunk} from './redux-store';
 
-export type AuthReducerACType = ReturnType<typeof setAuthUserData>
-
-type authReducerType =
-    AuthReducerACType
-
+export type AuthReducerACType = ReturnType<typeof setAuthUserData> | ReturnType<typeof stopSubmit>
 
 export type AuthInitialStateType = {
     userId: number | null,
     email: string | null,
     login: string | null,
     isAuth: boolean,
+    isStopSubmit: boolean,
+    stopSubmitMessage: string
 }
 
 let initialState: AuthInitialStateType = {
     userId: null,
     email: null,
     login: null,
-    isAuth: false
+    isAuth: false,
+    isStopSubmit: false,
+    stopSubmitMessage: ''
 }
 
-export const authReducer = (state: AuthInitialStateType = initialState, action: authReducerType): AuthInitialStateType => {
+export const authReducer = (state = initialState,
+                            action: AuthReducerACType): AuthInitialStateType => {
     switch (action.type) {
         case 'SET-USER-DATA':
-            return {...state, ...action.payload}
+            return {
+                ...state,
+                ...action.payload
+            }
+        case 'STOP-SUBMIT': {
+            return {
+                ...state,
+                isStopSubmit: action.payload.isStopSubmit,
+                stopSubmitMessage: action.payload.stopSubmitMessage
+            }
+        }
         default:
             return state;
     }
 }
 
-export const setAuthUserData = (userId: number| null, email: string | null, login: string| null, isAuth: boolean) => ({
+export const setAuthUserData = (userId: number | null, email: string | null,
+                                login: string | null, isAuth: boolean) => ({
     type: 'SET-USER-DATA',
-    payload: {userId, email, login, isAuth} as const
-})
-// export const setIsLoggedIn = (value: boolean) => ({
-//     type: 'login/SET-IS-LOGGED-IN', value
-// } as const)
+    payload: {userId, email, login, isAuth}} as const)
+
+export const stopSubmit = (isStopSubmit: boolean, stopSubmitMessage: string) => ({
+    type: 'STOP-SUBMIT',
+    payload: {isStopSubmit, stopSubmitMessage},
+} as const)
 
 export const getAuthUserData = () => {
-    return (dispatch: Dispatch<authReducerType>) => {
+    return (dispatch: Dispatch<AuthReducerACType>) => {
         authAPI.me()
             .then(response => {
                 if (response.data.resultCode === 0) {
@@ -50,30 +63,33 @@ export const getAuthUserData = () => {
             });
     }
 }
-
-export const login = (email: string, password: string, rememberMe?: boolean,captcha?:boolean):AppThunk  => {
+export const login = (email: string, password: string, rememberMe?: boolean,
+                      captcha?: boolean): AppThunk => {
     return (dispatch) => {
-        authAPI.login(email, password,rememberMe,captcha)
+        authAPI.login(email, password, rememberMe, captcha)
             .then(response => {
                 if (response.data.resultCode === 0) {
                     dispatch(getAuthUserData())
+                    dispatch(stopSubmit(false, ''))
+                } else {
+                    dispatch(stopSubmit(true, response.data.messages[0]))
                 }
             })
-            .catch((error)=>{
+            .catch((error) => {
                 console.log(error)
             })
     }
 }
 
-export const logout = ():AppThunk => {
+export const logout = (): AppThunk => {
     return (dispatch) => {
         authAPI.logout()
             .then(response => {
                 if (response.data.resultCode === 0) {
-                    dispatch(setAuthUserData(null,null,null,false))
+                    dispatch(setAuthUserData(null, null, null, false))
                 }
             })
-            .catch(error=>
-            console.log(error))
+            .catch(error =>
+                console.log(error))
     }
 }
