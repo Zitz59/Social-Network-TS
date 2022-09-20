8a42f63b64059c0b221c152dd1e9ddb42c2710e7
Zitz59 <dondongudon@gmail.com>
2022-09-20 19:44:22 +0300

    feat: create app-reducer

---
+++ src/redux/app-reducer.ts
@@ -0,0 +1,86 @@
+import {Dispatch} from 'redux';
+import {authAPI} from '../api/api';
+import {AppThunk} from './redux-store';
+
+export type AuthReducerACType = ReturnType<typeof setAuthUserData> | ReturnType<typeof stopSubmit>
+
+export type AppInitialStateType = {
+    initialized:boolean
+}
+
+let initialState: AppInitialStateType = {
+    initialized: false
+}
+
+export const appReducer = (state = initialState,
+                           action: AuthReducerACType): AppInitialStateType => {
+    switch (action.type) {
+        case 'SET-USER-DATA':
+            return {
+                ...state,
+                ...action.payload
+            }
+        case 'STOP-SUBMIT': {
+            return {
+                ...state,
+                isStopSubmit: action.payload.isStopSubmit,
+                stopSubmitMessage: action.payload.stopSubmitMessage
+            }
+        }
+        default:
+            return state;
+    }
+}
+
+export const setAuthUserData = (userId: number | null, email: string | null,
+                                login: string | null, isAuth: boolean) => ({
+    type: 'SET-USER-DATA',
+    payload: {userId, email, login, isAuth}
+} as const)
+
+export const stopSubmit = (isStopSubmit: boolean, stopSubmitMessage: string) => ({
+    type: 'STOP-SUBMIT',
+    payload: {isStopSubmit, stopSubmitMessage},
+} as const)
+
+export const getAuthUserData = () => {
+    return (dispatch: Dispatch<AuthReducerACType>) => {
+        authAPI.me()
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    let {id, email, login,} = response.data.data
+                    dispatch(setAuthUserData(id, email, login, true))
+                }
+            });
+    }
+}
+export const login = (email: string, password: string, rememberMe?: boolean,
+                      captcha?: boolean): AppThunk => {
+    return (dispatch) => {
+        authAPI.login(email, password, rememberMe, captcha)
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    dispatch(getAuthUserData())
+                    dispatch(stopSubmit(false, ''))
+                } else {
+                    dispatch(stopSubmit(true, response.data.messages[0]))
+                }
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+}
+
+export const logout = (): AppThunk => {
+    return (dispatch) => {
+        authAPI.logout()
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    dispatch(setAuthUserData(null, null, null, false))
+                }
+            })
+            .catch(error =>
+                console.log(error))
+    }
+}
